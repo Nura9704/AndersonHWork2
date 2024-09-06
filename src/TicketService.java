cf5457dd03012d013b54a94b6324ffd632bd47dd
Nura9704 <nurasil9704@gmail.com>
2024-09-06 20:57:44 +0500

    make another method by request

--- src/TicketService.java
+++ src/TicketService.java
@@ -4,17 +4,13 @@ public class TicketService {
 
     public static void main(String[] args) {
         try {
-            // Create an empty ticket
             Ticket emptyTicket = new Ticket();
 
-            // Create a ticket with all parameters
-            Ticket fullTicket = new Ticket("1234", "ConcertHall", EventCode.CONCERT_101, 1700000000L,
+            Ticket fullTicket = new Ticket("1234", "LargeHall", EventCode.CONCERT_101, 1700000000L,
                     PromoStatus.PROMO, StadiumSector.B, 7.5, 50.0);
 
-            // Create a ticket with limited parameters
             Ticket limitedTicket = new Ticket("SmallHall", EventCode.CONCERT_202, 1700001000L);
 
-            // Print details of the tickets using toString method
             System.out.println("Empty Ticket: " + emptyTicket);
             System.out.println("Full Ticket: " + fullTicket);
             System.out.println("Limited Ticket: " + limitedTicket);
@@ -22,7 +18,6 @@ public class TicketService {
             System.err.println("Error creating ticket: " + e.getMessage());
         }
 
-        //10 ticket storage
         tickets[0] = new Ticket("0001", "LargeHall", EventCode.CONCERT_101, 1700000000L, PromoStatus.PROMO, StadiumSector.A, 7.5, 50.0);
         tickets[1] = new Ticket("0002", "SmallHall", EventCode.CONCERT_202, 1700001000L, PromoStatus.NO_PROMO, StadiumSector.B, 7.1, 45.0);
         tickets[2] = new Ticket("0003", "SmallHall", EventCode.CONCERT_202, 1700005000L, PromoStatus.PROMO, StadiumSector.C, 7.4, 54.0);
@@ -35,15 +30,11 @@ public class TicketService {
         tickets[9] = new Ticket("0010", "SmallHall", EventCode.CONCERT_101, 1700009000L, PromoStatus.PROMO, StadiumSector.C, 7.2, 45.0);
 
         Ticket ticket = getTicketById("0001");
-        if (ticket != null) {
-            System.out.println("Found Ticket: " + ticket);
-        } else {
-            System.out.println("Ticket with the given ID not found.");
-        }
+        isTicketFound(ticket);
 
     }
 
-    // get a ticket by its ID
+
     private static Ticket getTicketById(String id) {
         for (Ticket ticket : tickets) {
             if (ticket != null && id.equals(ticket.id)) {
@@ -51,6 +42,14 @@ public class TicketService {
             }
         }
         return null;
+    }
+
+    private static void isTicketFound(Ticket ticket) {
+        if (ticket != null) {
+            System.out.println("Found Ticket: " + ticket);
+        } else {
+            System.out.println("Ticket with the given ID not found.");
+        }
     }
 
 }
